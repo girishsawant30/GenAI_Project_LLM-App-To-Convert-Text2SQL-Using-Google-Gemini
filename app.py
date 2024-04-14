be7a9c7d95bb7d47d5df67d8f54d974e8609751a
girishsawant30 <87440249+girishsawant30@users.noreply.github.com>
2024-04-14 17:42:15 +0530

    Final Commit

--- app.py
+++ app.py
@@ -66,10 +66,25 @@ def read_sql_query(sql, connection_string):
             connection.close()
 
 ## Define Your Prompt
-prompt ="""You are an expert in converting English questions to SQL query!
+promp1 ="""You are an expert in converting English questions to SQL query!
 The SQL database has the name AdventureWorks2017 and the table name is Employees 
 under schema dbo and has the following columns - ID, FirstName, LastName, Gender, Salary
 For example,\nExample 1 - How many count of records are present?, 
+the SQL command will be something like this SELECT COUNT(*) FROM AdventureWorks2017.dbo.Employees;
+Example 2 - Tell me person name with the highest salary?, 
+the SQL command will be something like this SELECT TOP 1 FirstName, LastName FROM AdventureWorks2017.dbo.Employees order by Salary desc; 
+also the sql code should not have ``` in beginning or end and sql word in output. Do not makeup answer, if you are unable to understand
+the answer then just say that you do not know the answer. Do not give wrong answers validate once before giving them.
+"""
+
+#(To get complex answers like inner join)
+prompt ="""You are an expert in converting English questions to SQL query!
+The SQL database has the name AdventureWorks2017 and there are two tables named Person & PersonPhone
+under schema Person and has the following columns in 
+Person table - BusinessEntityID, Title, FirstName, LastName, Demographics, EmailPromotion etc
+and 
+PersonPhone table - BusinessEntityID, PhoneNumber, where BusinessEntityID is primarykey of Person table & PersonPhone has BusinessEntityID as foreign key
+I need your help to write basic to complex queries For example,\nExample 1 - How many count of records are present?, 
 the SQL command will be something like this SELECT COUNT(*) FROM AdventureWorks2017.dbo.Employees;
 Example 2 - Tell me person name with the highest salary?, 
 the SQL command will be something like this SELECT TOP 1 FirstName, LastName FROM AdventureWorks2017.dbo.Employees order by Salary desc; 
@@ -98,9 +113,15 @@ if submit:
     # Execute the SQL query and display results
     result = read_sql_query(response, connection_string)
     
-    st.subheader("The Response is:")
-    for row in result:
-        print(row)
-        st.header(row)
+    if result is not None:
+        st.subheader("The Response is:")
+        for row in result:
+            # Remove the trailing comma from the tuple and convert it to a string
+            row_str = str(row).replace(",", "")
+            print(row_str)
+            st.header(row_str)
+    else:
+        st.error("No results found for the query.")
 
-
+#provide me phonenumber of person FirstName = 'ken' in person.person table by joining it with the  person.personphone table
+#provide me count of persons in persons.persons table with the FirstName = 'ken' and lastname as 'Sánchez'
